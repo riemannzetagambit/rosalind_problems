c635905bf9e86c8b0f3b58fa300d95cf20ca1623
David Stone <dstone@tracegenomics.com>
2018-11-16 22:51:20 -0800

    Code for prob problem, fixing relative directories with test suite and simplifying relative imports

---
+++ rosalind_prob.py
@@ -0,0 +1,45 @@
+from functools import reduce
+from math import log
+import operator
+import sys
+
+from .rosalind_utils import get_rosalind_data
+
+
+# this is not a python builtin, so make our own
+# see https://stackoverflow.com/questions/595374/whats-the-function-like-sum-but-for-multiplication-product
+def prod(iterable):
+    return reduce(operator.mul, iterable, 1)
+
+def solve_problem(sequence_data):
+    '''
+    Assumptions: file is of the form
+    >read_id_0
+    [arbitrary number of lines of ATCG]
+    >read_id_1
+    [arbitrary number of lines of ATCG]
+
+    '''
+    read, probs = sequence_data[0], sequence_data[1]
+    # split space-delimited probabilities into list and cast them to floats with an iterable
+    probs = map(float, probs.split())
+
+    log_prob_random_str = []
+    for prob in probs:
+        # the given probability is the GC content, so prob(G) = prob(c) = prob/2
+        prob_dict['C'], prob_dict['G'] = prob/2
+        # by conservation of probability
+        prob_dict['A'], prob_dict['C'] = (1 - prob)/2
+        # multiply together the probability of each individual nt
+        prob_read = prod([prob_dict[x] for x in read])
+        log_prob_random_str.append(log(prob_read, base=10))
+
+    print(' '.join(log_prob_random_str))
+    return ' '.join(log_prob_random_str)
+
+
+if __name__ == '__main__':
+    args = list(sys.argv)
+    if len(args) != 2:
+        raise ValueError('Must provide a single argument which is the data file path')
+    solve_problem(get_rosalind_data(args[-1]))
--- tests/test_rosalind.py
+++ tests/test_rosalind.py
@@ -1,16 +1,29 @@
 import os
+import sys
 
-from ..rosalind_utils import get_rosalind_data
-from ..rosalind_corr import solve_problem as corr_solve
-from ..rosalind_long import solve_problem as long_solve
+# remove the need for relative .s and add directory above tests to path
+sys.path.insert(0, '..')
 
+from rosalind_utils import get_rosalind_data
+from rosalind_corr import solve_problem as corr_solve
+from rosalind_long import solve_problem as long_solve
+#from ..rosalind_prob import solve_problem as prob_solve
 
-def test_rosalind_long():
-    solution = 'ATTAGACCTGCCGGAATAC'
-    test_data = get_rosalind_data(os.path.abspath('./rosalind_long_test.txt'))
-    assert long_solve(test_data) == solution
+file_path = os.path.dirname(os.path.abspath(__file__))
 
 def test_rosalind_corr():
     solution = 'TTCAT->TTGAT\nGAGGA->GATGA\nTTTCC->TTTCA'
-    test_data = get_rosalind_data(os.path.abspath('./rosalind_corr_test.txt'))
+    test_path = os.path.join(file_path, './rosalind_corr_test.txt')
+    test_data = get_rosalind_data(test_path)
     assert corr_solve(test_data) == solution
+
+def test_rosalind_long():
+    solution = 'ATTAGACCTGCCGGAATAC'
+    test_path = os.path.join(file_path, './rosalind_long_test.txt')
+    test_data = get_rosalind_data(test_path)
+    assert long_solve(test_data) == solution
+
+#def test_rosalind_prob():
+#    solution = '-5.737 -5.217 -5.263 -5.360 -5.958 -6.628 -7.009'
+#    test_data = get_rosalind_data(os.path.abspath('./rosalind_prob_test.txt'))
+#    assert prob_solve(test_data) == solution
